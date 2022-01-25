483543560fc971735996a3a0237423e55f870cba
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2022-01-25 10:24:46 +0900

    Show the IP address of host machine

--- port-forward-on.bat
+++ port-forward-on.bat
@@ -1,8 +1,9 @@
 FOR /F "usebackq" %%i in (`wsl exec hostname -I`) DO @SET IP=%%i
 netsh interface portproxy add v4tov4 listenport=8000 connectaddress=%IP%
 netsh.exe interface portproxy show v4tov4
+
+ipconfig
 pause
 
 netsh.exe interface portproxy delete v4tov4 8000
 netsh.exe interface portproxy show v4tov4
-pause
